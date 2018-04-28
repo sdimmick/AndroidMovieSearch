ab76e1920e22f93fd3ab7abc08d4a59b43fa5343
Stephen Dimmick <stephen.dimmick@gmail.com>
2018-04-28 11:40:25 -0700

    Clean up tab nav bar

--- app/src/main/java/com/sdimmick/androidmoviesearch/MainActivity.java
+++ app/src/main/java/com/sdimmick/androidmoviesearch/MainActivity.java
@@ -17,14 +17,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_search:
+                    mTextMessage.setText(R.string.title_search);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_favorites:
+                    mTextMessage.setText(R.string.title_favorites);
                     return true;
             }
             return false;
@@ -36,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
